c5fc9288fafde8573522c1eeb4b28bc4aeab2cb2
John Oblenda <joandong2@gmail.com>
2020-05-11 23:36:01 -0400

    added dependencies

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,25 +1,35 @@
-import React from 'react';
+import React from "react";
 
-const MovieCard = props => {
-  const { title, director, metascore, stars } = props.movie;
-  return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
+const MovieCard = (props) => {
+    console.log("moviecard", props);
+    const { id, title, director, metascore, stars } = props.movie;
+    return (
+        <div className="movie-card">
+            <h2>{title}</h2>
+            <div className="movie-director">
+                Director: <em>{director}</em>
+            </div>
+            <div className="movie-metascore">
+                Metascore: <strong>{metascore}</strong>
+            </div>
+            <h3>Actors</h3>
 
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
+            {stars.map((star) => (
+                <div key={star} className="movie-star">
+                    {star}
+                </div>
+            ))}
+            <button
+                onClick={() => props.history.push(`/update-movie/${id}`)}
+                className="md-button"
+            >
+                Edit
+            </button>
+            {/* <button onClick={deleteItem} className="md-button">
+                Delete
+            </button> */}
         </div>
-      ))}
-    </div>
-  );
+    );
 };
 
 export default MovieCard;
---
+++ client/src/Movies/UpdateMovie.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+
+const UpdateMovie = (props) => {
+    return <h1>{props.movie}</h1>;
+};
+
+export default UpdateMovie;
